5866a1e0e9be0f4cca63bf90bb1c977d76e741f6
Xavier Cho <mysticfallband@gmail.com>
2021-10-04 06:44:04 +0900

    Put documentation path to .prettierignore

--- src/common/SectionTitle.js
+++ src/common/SectionTitle.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types'
 import tw from 'tailwind-styled-components'
 
-const Heading = tw.h2`font-display font-bold text-gray-800 text-2xl mb-2`
+const Heading = tw.h2`font-display font-bold text-gray-800 dark:text-white text-2xl mb-2`
 
 const SectionTitle = ({ children, ...rest }) => {
   return <Heading {...rest}>{children}</Heading>
