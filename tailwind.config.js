f07e8188df3336a94fb3dd1efb9a3e82d171327b
Xavier Cho <mysticfallband@gmail.com>
2021-10-10 12:30:46 +0900

    Add additional colours/spacing and update safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,20 @@
-const { fontFamily, height } = require('tailwindcss/defaultTheme')
+const { fontFamily, height, spacing } = require('tailwindcss/defaultTheme')
 
 module.exports = {
   mode: 'jit',
-  purge: ['./public/**/*.html', './src/**/*.js'],
+  purge: {
+    content: ['./public/**/*.html', './src/**/*.js'],
+    options: {
+      safelist: ['opacity-0', 'opacity-100'],
+    },
+  },
   darkMode: 'class',
   theme: {
     extend: {
       colors: {
         upbge: '#f99543',
         'gray-150': '#B5B5B5',
+        'gray-650': '#333333',
         'gray-930': '#222222',
         'gray-950': '#121212',
       },
@@ -19,6 +25,10 @@ module.exports = {
       height: {
         18: '4.5em',
         ...height,
+      },
+      spacing: {
+        18: '4.5em',
+        ...spacing,
       },
       transitionProperty: {
         height: 'height',
