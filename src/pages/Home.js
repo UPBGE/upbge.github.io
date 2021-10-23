203eed858e7ab01b0a512dfc4c258b119fb37b8d
Xavier Cho <mysticfallband@gmail.com>
2021-10-23 13:31:06 +0900

    Add contents from V's final version to the main page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,8 @@ import SectionTitle from '../common/SectionTitle'
 import banner from '../assets/videos/banner.mp4'
 import screenshot1 from '../assets/images/evee.gif'
 import screenshot2 from '../assets/images/logicnode.gif'
+import screenshot3 from '../assets/images/custominterface.gif'
+import screenshot4 from '../assets/images/games.gif'
 
 const Video = tw.video`absolute top-0 left-0 w-screen h-screen object-cover object-center bg-black`
 const Jumbotron = tw.div`absolute top-0 left-0 w-screen h-screen max-w-full z-40 text-white flex 
@@ -16,9 +18,11 @@ const Content = tw.div`px-6 md:px-12`
 const Intro = tw(
   Content
 )`bg-gray-50 dark:bg-gray-950 text-center py-10 md:py-16`
-const Section = tw(
-  Content
-)`md:grid md:grid-cols-2 py-4 md:py-10 space-x-2 max-w-6xl`
+
+const Section = tw(Content)`py-4 md:py-10`
+const HSection = tw(Section)`md:grid md:grid-cols-2 space-x-2 max-w-6xl`
+
+const BulletList = tw.ul`list-disc p-4 md:p-0`
 
 const Home = () => {
   const marqueeTexts = ['Game', 'Scripts', 'Models', 'Textures', 'Materials']
@@ -66,11 +70,11 @@ const Home = () => {
       </Intro>
 
       <div className={'flex flex-col items-center'}>
-        <Section>
+        <HSection>
           <div>
             <SectionTitle>Basic Features</SectionTitle>
 
-            <ul className={'list-disc p-4 md:p-0'}>
+            <BulletList>
               <li>
                 Very fast workflow with the convenience of Blender internal &
                 Cycles nodes.
@@ -84,18 +88,18 @@ const Home = () => {
                 supporting 3D sound and sound effects.
               </li>
               <li>Easy and straightforward visual logic system.</li>
-            </ul>
+            </BulletList>
           </div>
           <div className={'md:order-first'}>
             <img src={screenshot1} alt={'UPBGE Screenshot #1'} />
           </div>
-        </Section>
+        </HSection>
 
-        <Section>
+        <HSection>
           <div>
             <SectionTitle>Advanced Features</SectionTitle>
 
-            <ul className={'list-disc p-4 md:p-0'}>
+            <BulletList>
               <li>PBR materials.</li>
               <li>
                 Powerful Python language bindings and components, allowing more
@@ -107,11 +111,115 @@ const Home = () => {
                 interface.
               </li>
               <li>HDR pipeline.</li>
-            </ul>
+            </BulletList>
           </div>
           <div>
             <img src={screenshot2} alt={'UPBGE Screenshot #2'} />
           </div>
+        </HSection>
+
+        <Section
+          className={
+            'max-w-3xl text-left md:text-center flex flex-col space-y-6 align-center'
+          }>
+          <div>
+            <SectionTitle>Fully Customizable Interface</SectionTitle>
+
+            <p>
+              Here you are free to leave the interface as you like, don&apos;t
+              limit yourself to themes that only change the color, add, remove,
+              scale areas to look like you always dreamed of!
+            </p>
+          </div>
+          <div>
+            <img src={screenshot3} alt={'UPBGE Screenshot #3'} width={1024} />
+          </div>
+        </Section>
+
+        <Section className={'max-w-7xl'}>
+          <div className={'grid grid-cols-1 lg:grid-cols-3 gap-x-10'}>
+            <div>
+              <SectionTitle className={'text-left md:text-center'}>
+                Integrated with Blender
+              </SectionTitle>
+
+              <BulletList>
+                <li>
+                  Real time collaboration of game developers through the Mixer
+                  addon. It allows multiple UPBGE game developers to work over
+                  the same scene at the same time increasing the productivity of
+                  small studios distributed in different places.
+                </li>
+                <li>
+                  Simply pressing &quot;P&quot; key and you are inside of your
+                  game testing your changes.
+                </li>
+                <li>
+                  All the new features for Blender are automatically included in
+                  each UPBGE release.
+                </li>
+                <li>
+                  The best game design pipeline ever, thanks to its built-in 3D
+                  tools package. The game engine&apos;s specific workspace,
+                  editors, panels or nodes follow the same convection and
+                  patterns as Blender itself.
+                </li>
+              </BulletList>
+            </div>
+
+            <div>
+              <SectionTitle className={'text-left md:text-center'}>
+                Graphics
+              </SectionTitle>
+
+              <BulletList>
+                <li>
+                  Great color management, including HDR, tone mapping, exposure
+                  and color transformations such as Filmic.
+                </li>
+                <li>Physically-based rendering with TAA and SMAA</li>
+                <li>Principled BSDF</li>
+
+                <li>Environment lighting and HDRIs</li>
+                <li>Screen-space reflections and refractions</li>
+                <li>Indirect light through light probes</li>
+                <li>Soft and contact shadows</li>
+                <li>Subsurface scattering and volume rendering</li>
+                <li>Bloom</li>
+                <li>Volumetrics</li>
+                <li>Depth of field</li>
+              </BulletList>
+            </div>
+
+            <div>
+              <SectionTitle className={'text-left md:text-center'}>
+                Physics/Animation
+              </SectionTitle>
+
+              <BulletList>
+                <li>Rigid bodies</li>
+                <li>Dynamic bodies</li>
+                <li>Soft bodies</li>
+                <li>Character controller</li>
+                <li>Static objects</li>
+                <li>Keyframes</li>
+                <li>Armatures</li>
+                <li>Shape keys</li>
+                <li>Drivers</li>
+                <li>Actions</li>
+                <li>motion capture</li>
+              </BulletList>
+            </div>
+          </div>
+        </Section>
+
+        <Section
+          className={
+            'max-w-3xl text-left md:text-center flex flex-col space-y-6 align-center md:py-4'
+          }>
+          <SectionTitle>Community Games in Development</SectionTitle>
+
+          <img src={screenshot4} alt={'UPBGE Screenshot #4'} width={1024} />
         </Section>
       </div>
     </section>
