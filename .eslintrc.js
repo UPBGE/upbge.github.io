b9c525a7657733703a98bd6c04c6fd24d19ce61f
Xavier Cho <mysticfallband@gmail.com>
2021-10-24 06:44:41 +0900

    Update NPM dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,8 +8,7 @@ module.exports = {
     extends: [
         "eslint:recommended",
         "plugin:react/recommended",
-        "prettier",
-        "prettier/react"
+        "prettier"
     ],
     parserOptions: {
         ecmaVersion: "2017",
