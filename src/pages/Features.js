546e51d4a6d0da3857e66a2e012dd765de3d24e6
Leo <lauersperg@gmx.at>
2021-11-17 11:42:39 +0100

    Fix Typos, adapt some text

--- src/pages/Features.js
+++ src/pages/Features.js
@@ -24,30 +24,34 @@ const Features = () => {
           <div>
             <SectionTitle>Integrated Design in Blender</SectionTitle>
             <p>
-              UPBGE is a game engine build over the famous Blender 3d tool. It
+              UPBGE is a game engine built over the famous Blender 3D tool. It
               is internally integrated in such a way that what you see in the
-              Blender viewport is what you will see in the game engine.
+              Blender viewport (EEVEE) is what you will see in-game.
             </p>
             <p>
               <br />
-              Thanks to this integration we have that:
+              Thanks to this integration we have:
             </p>
             <BulletList>
               <li>
-                No need for an import-export pipeline. Simply pressing
-                &quot;P&quot; key and you are inside of your game testing your
-                changes. This makes the feedback instantaneous, both visually
-                and in terms of the game logic development.
+                No need for an import-export pipeline. Since Blender supports
+                3D modelling, sculpting and UV mapping, everything made in the
+                UPBGE can be used "as is".
               </li>
               <li>
-                All the new features for Blender are automatically included in
-                each UPBGE release.
+                Rapid iteration. Simply pressing the &quot;P&quot; key will
+                start your game in the active viewport, allowing you to get
+                instant feedback both visually and in terms of gameplay and
+                logic.
               </li>
               <li>
-                Best game design pipeline ever because there is no need to adapt
-                an external 3d tool package. The game engine specific workspace,
-                editors, panels or nodes follow the same convection and
-                standards than Blender itself.
+                Implicit updates. All the new features for Blender are
+                automatically included in each UPBGE release.
+              </li>
+              <li>
+                A unified workflow. The game engine specific workspaces,
+                editors, panels and nodes follow the same conventions and
+                standards as Blender itself.
               </li>
             </BulletList>
           </div>
@@ -93,9 +97,9 @@ const Features = () => {
             <SectionTitle>Scripting & Logic Support</SectionTitle>
             <p>
               The UPBGE scripting language is Python. UPBGE has a very complete
-              Python API that can be used as in the embedded editor as in the
-              player. This scripting language is the language preferred to
-              develop the game logic although there are other 3 additional and
+              Python API that can be used in the embedded editor as well as
+              in-game. This scripting language is the most grounded way to
+              develop game logic, although there are 3 additional and
               complete methods that the game developer can use:
             </p>
             <BulletList>
@@ -108,17 +112,17 @@ const Features = () => {
               </li>
               <li>
                 <p>The Logic Nodes system.</p> This system is a Visual Scripting
-                system developed over the UPBGE node interface to create
+                system developed atop the UPBGE node interface to create
                 gameplay elements inside UPBGE editor. Its main advantages are
                 its versatility and ease of use. No programming knowledge is
                 required to use them.
               </li>
               <li>
                 <p>The Python Components system.</p> Basically the Python
-                Components are modules that can be attached to game objects and
-                each one serves for a specific purpose. No programming knowledge
+                Components are modules that can be attached to game objects.
+                Each one serves a specific purpose. No programming knowledge
                 is required to use them, although such knowledge is necessary
-                for the development of new components.
+                for the development of new or custom components.
               </li>
             </BulletList>
           </div>
@@ -131,21 +135,21 @@ const Features = () => {
           <div>
             <SectionTitle>Animation System</SectionTitle>
             <p>
-              The UPBGE animation system is the same than the Blender one. Due
-              to this, UPBGE has one of the most powerfull animation system out
+              The UPBGE animation system is the same as the Blender one. Due
+              to this, UPBGE has one of the most powerful animation systems out
               there.
             </p>
             <p>
-              Almost all the properties of UPBGE can be animated, from basic
-              motion/rotation animation, through sprite animation and ending
-              with different node properties of both materials and geometry or
-              even grease pencil objects.
+              Almost all properties of UPBGE can be animated. From basic
+              motion/rotation animations through sprite animation all the way to
+              different node properties of both materials and geometry node
+              trees. Even grease pencil objects can be animated.
             </p>
 
             <p>
               <br />
-              Basically to make beautiful animations the developer can use any
-              of the following systems:
+              To make animations, the developer can use any of the following
+              systems supported by the game engine:
             </p>
             <BulletListInside>
               <li>Keyframes.</li>
@@ -173,17 +177,17 @@ const Features = () => {
             <GridSection>
               <GridSectionTitle>Physics</GridSectionTitle>
               <p>
-                UPBGE includes advanced physics simulation in the form of the
-                Bullet Physics Engine (Bullet Physics). Most of your work will
-                involve setting the right properties on the objects in your
-                scene, then you can sit back and let the engine take over. The
-                physics simulation can be used for games, but also for
-                animation.
+                UPBGE includes advanced physics simulation as it incorperates
+                the Bullet Physics Engine (Bullet Physics). Most of your work
+                will come down to setting the right properties on the objects in
+                your scene before you can sit back and let the engine take over.
+                The physics simulation can be used for games as well as
+                animations.
               </p>
               <p>
                 <br />
-                UPBGE physics system can use Rigid, Dynamics or Soft bodies,
-                Static Objects and Kinematic Character controller.
+                UPBGE physics system supports Rigid, Dynamic and Soft bodies,
+                static objects and a kinematic character controller.
               </p>
             </GridSection>
 
@@ -219,13 +223,13 @@ const Features = () => {
             <GridSection>
               <GridSectionTitle>Navigation System</GridSectionTitle>
               <p>
-                UPBGE includes a state of the art navigation system based in the
+                UPBGE includes a state of the art navigation system based on the
                 Recast & Detour libraries.
               </p>
               <p>
                 <br />
-                It offers simple static navigation mesh which is suitable for
-                many simple cases and a navigation mesh construction toolset.
+                It offers simple static navigation mesh support suitable for
+                many cases and comes with a navmesh generation toolset.
               </p>
             </GridSection>
 
